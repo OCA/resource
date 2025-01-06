6c3a2aea8414c1b80367a352b4095d0b158cf128
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-01-06 11:40:58 -0500

    [16.0][ADD] hr_planning_oca
    [16.0][ADD] hr_planning_oca

---
+++ hr_planning_oca/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "HR Planner OCA",
+    "summary": "",
+    "version": "16.0.1.0.1",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/resource",
+    "author": "Binhex, Odoo Community Association (OCA)",
+    "depends": [
+        "project",
+        "web_timeline",
+        "helpdesk_mgmt",
+        "hr_holidays",
+    ],
+    "data": ["views/menus.xml"],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
--- hr_planning_oca/readme/CONTRIBUTORS.rst
+++ hr_planning_oca/readme/CONTRIBUTORS.rst
@@ -1,5 +1,4 @@
 * `Binhex <https://www.binhex.cloud>`_:
 
   * Antonio Ruban
-  * Adasat Torres de Leon
   * Zuzanna Elzbieta Szalaty Szalaty
